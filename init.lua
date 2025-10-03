81727c40bd0a77f86174bdf7e2abcfd8c124fdc2
Máté Mészáros <meszmatew@gmail.com>
2025-10-03 10:47:22 +0200

    icons

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
@@ -44,4 +44,3 @@ require('lazy').setup({
     },
   },
 })
-
