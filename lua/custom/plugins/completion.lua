e1bf2a7f8a61cf807df824e4e7d4c1306b8a4304
Máté Mészáros <meszmatew@gmail.com>
2025-10-24 19:35:53 +0200

    cmp details

--- lua/custom/plugins/completion.lua
+++ lua/custom/plugins/completion.lua
@@ -1,32 +1,39 @@
 return {
-    { 'saghen/blink.cmp',
-      event = 'VimEnter',
-      version = '1.*',
-      dependencies = {
-        {
-          'L3MON4D3/LuaSnip',
-          version = '2.*',
-          build = (function()
-            if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-              return
-            end
-            return 'make install_jsregexp'
-          end)(),
-          opts = {},
-        },
-        'folke/lazydev.nvim',
+  {
+    'saghen/blink.cmp',
+    event = 'VimEnter',
+    version = '1.*',
+    dependencies = {
+      {
+        'L3MON4D3/LuaSnip',
+        version = '2.*',
+        build = (function()
+          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+            return
+          end
+          return 'make install_jsregexp'
+        end)(),
+        opts = {},
       },
-      opts = {
-        keymap = { 
-            preset = 'default',
-        },
-
-        appearance = { 
-            use_nvim_cmp_as_default = true,
-            nerd_font_variant = 'mono' 
-        },
-        
-        signature = { enabled = true },
-      },
+      'folke/lazydev.nvim',
     },
+    opts = {
+      keymap = {
+        preset = 'default',
+      },
+
+      completion = {
+        documentation = { auto_show = true, auto_show_delay_ms = 500 },
+      },
+
+      appearance = {
+        use_nvim_cmp_as_default = true,
+        show_label_details = true,
+        nerd_font_variant = 'mono',
+      },
+
+      signature = { enabled = true },
+    },
+  },
 }
+
